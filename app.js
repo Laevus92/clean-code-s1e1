51fd3caef0a73e21b8b49b5e59bc7d2851912d39
Laevus92 <laevus@yandex.ru>
2023-04-18 18:04:06 +0300

    fix(advanced-1.2): added alt attribute for img tags in html also added alt while creating delete button in js

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="task__delete-btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='remove_button.svg'
     deleteButtonImg.className='delete-btn__image'
     deleteButton.appendChild(deleteButtonImg);
 
